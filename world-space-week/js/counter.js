b4b54cc69478df9092beb1215ee98baa073c31ac
Imashi <2018is016@stu.ucsc.cmb.ac.lk>
2021-10-10 09:42:31 +0530

    counter updated

--- world-space-week/js/counter.js
+++ world-space-week/js/counter.js
@@ -1,9 +1,7 @@
-// The data/time we want to countdown to
 var countDownDate = new Date("Oct 10, 2021 23:59:59").getTime();
+var surveyDeadline = new Date("Oct 11, 2021 23:59:59").getTime();
 
-// Run myfunc every second
-var myfunc = setInterval(function () {
-
+var myfunc = setInterval(function () {// Run myfunc every second
     var now = new Date().getTime();
     var timeleft = countDownDate - now;
 
@@ -18,8 +16,7 @@ var myfunc = setInterval(function () {
     document.getElementById("poster-mins").innerHTML = minutes + "m :"
     document.getElementById("poster-secs").innerHTML = seconds + "s "
 
-    // Display the message when countdown is over
-    if (timeleft < 0) {
+    if (timeleft < 0) {// Display the message when countdown is over
         clearInterval(myfunc);
         document.getElementById("poster-days").innerHTML = ""
         document.getElementById("poster-hours").innerHTML = ""
@@ -29,30 +26,20 @@ var myfunc = setInterval(function () {
     }
 }, 1000);
 
-
-
-// The data/time we want to countdown to
-var surveyDeadline = new Date("Oct 11, 2021 23:59:59").getTime();
-
-// Run myfunc every second
 var myfunction = setInterval(function () {
-
     var now = new Date().getTime();
     var timeleft = surveyDeadline - now;
 
-    // Calculating the days, hours, minutes and seconds left
     var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
     var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-    // Result is output to the specific element
     document.getElementById("survey-days").innerHTML = days + "d :"
     document.getElementById("survey-hours").innerHTML = hours + "h :"
     document.getElementById("survey-mins").innerHTML = minutes + "m :"
     document.getElementById("survey-secs").innerHTML = seconds + "s "
 
-    // Display the message when countdown is over
     if (timeleft < 0) {
         clearInterval(myfunction);
         document.getElementById("survey-days").innerHTML = ""
